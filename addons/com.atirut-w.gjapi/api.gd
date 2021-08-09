3a489fa2bbf2716011d82afed4b583dd2c083116
Atirut Wattanamongkol <artjang301@gmail.com>
2021-08-09 22:25:16 +0700

    Authentication

--- addons/com.atirut-w.gjapi/api.gd
+++ addons/com.atirut-w.gjapi/api.gd
@@ -3,13 +3,36 @@ extends Node
 var game_id := ""
 var game_key := ""
 
+var _username := ""
+var _token := ""
+var _is_authenticated := false
+
 const base_url := "api.gamejolt.com/api/game"
 var api_version = api_versions.v1_2
 const api_versions = {
 	v1_2 = "v1_2"
 }
 
+## Bool return indicate whether the auth is successful or not.
+func authenticate(username: String, token: String) -> bool:
+	require_game_id()
+	var response = yield(_send_api_request("users/auth/", {game_id = game_id, username = username, user_token = token}), "completed")
+	if response:
+		_username = username
+		_token = token
+		_is_authenticated = true
+		return true
+	else:
+		_username = ""
+		_token = ""
+		_is_authenticated = false
+		return false
+
+func require_game_id() -> void:
+	assert(game_id != "", "game ID is not set")
+
 func _send_api_request(endpoint: String, queries := {}) -> Dictionary:
+	assert(game_key != "", "game key is not set") # This is not in it's own function because every API call have to be signed.
 	# Any double slashes after HTTP:// can ruin the signature.
 	var url = "https://" + ("%s/%s/%s" % [base_url, api_version, endpoint]).replace("//", "/")
 	
